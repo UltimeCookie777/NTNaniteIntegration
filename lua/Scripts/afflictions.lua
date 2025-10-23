cfade55e28c8390f512f899075f935e150f390c6
Ultime_Cookie777 <trove.jumpx@gmail.com>
2025-10-23 13:02:20 +0200

    added all 'healing only' nanites

--- lua/Scripts/afflictions.lua
+++ lua/Scripts/afflictions.lua
@@ -7,7 +7,7 @@ NTNan.Afflictions.Reconstructor = "reconstructornanite"
 NTNan.Afflictions.DeepFix = "deepfixnanite"
 NTNan.Afflictions.BoneFix = "bonefixnanite"
 NTNan.Afflictions.OxyBlood = "oxybloodnanite"
-NTNan.Afflictions.HuskPara = "huskparalnanite"
+NTNan.Afflictions.Husk = "huskparalnanite"
 NTNan.Afflictions.Neural = "neuralnanite"
 NTNan.Afflictions.AntiTox = "antitoxnanite"
 NTNan.Afflictions.VigorBuff = "vigorbuffnanite"
@@ -15,8 +15,33 @@ NTNan.Afflictions.HyperBuff = "hyperbuffnanite"
 NTNan.Afflictions.VisionBuff = "visionbuffnanite"
 NTNan.Afflictions.LaniusXeno = "laniusxenonanite"
 
-NTNan.AfflictionsRes.Reconstructor = { name = NTNan.Afflictions.Reconstructor, levels = {
-    {{"burn", 0.6},{"acidburn",0.3},{"lacerations",0.6}},
-    {{"bleeding",1},{"bleedingnonstop",1},{"bitewounds",0.6}},
-    {{"gunshotwounds",0.4}}}
+NTNan.AfflictionsRes[NTNan.Afflictions.Reconstructor] = { name = NTNan.Afflictions.Reconstructor, levels = {
+    {{"burn", 0.3},{"acidburn",0.15},{"lacerations",0.3}},
+    {{"bleeding",0.5},{"bleedingnonstop",0.5},{"bitewounds",0.3}},
+    {{"gunshotwounds",0.2}}}
 }
+
+NTNan.AfflictionsRes[NTNan.Afflictions.BoneFix] = { name = NTNan.Afflictions.BoneFix, levels = {
+    {{"blunttrauma", 0.4},{"ra_fracture", 3}, {"la_fracture", 3}},
+    {{"rl_fracture", 3},{"ll_fracture", 3},{"t_fracture",3}},
+    {{"n_fracture", 3},{"h_fracture",3},{"t_paralysis",1},{"bonedama",0.5}}}
+}
+
+NTNan.AfflictionsRes[NTNan.Afflictions.DeepFix] = { name = NTNan.Afflictions.DeepFix, levels = {
+    {{"internalbleeding", 0.25},{"foreignbody", 0.2}},
+    {{"organdamage", 0.1},{"liverdamage",0.05},{"heartdamage",0.05},{"lungdamage",0.05},{"kidneydamage",0.05},{"liverdamage",0.05}}}
+}
+
+NTNan.AfflictionsRes[NTNan.Afflictions.OxyBlood] = {name=NTNan.Afflictions.OxyBlood, levels = {
+    {{"bloodloss", 0.2},{"stun", 1.1},{"oxygenlow",0.4},{"radiationsickness",0.2},{"hypoxemia",0.3}}}
+}
+
+NTNan.AfflictionsRes[NTNan.Afflictions.AntiTox] = {name=NTNan.Afflictions.AntiTox, levels = {
+    {{"drunk",0.5},{"opiateoverdose",1},{"morbusinepoisoning",1.2},{"deliriumpoisoning",1.2},{"sufforinpoisoning",2.1},{"cyanidepoisoning",1.1},{"paralysis",1.1}}}
+}
+
+NTNan.AfflictionsRes[NTNan.Afflictions.Neural] = {name=NTNan.Afflictions.Neural, levels = {
+    {{"psychosis",0.2},{"hallucinating",0.2},{"fibrillation",1}},
+    {{"opiateaddiction",0.2},{"chemaddiction",0.2},{"cerebralhypoxia",0.15},{"cardiacarrest",2}}}
+}
+
