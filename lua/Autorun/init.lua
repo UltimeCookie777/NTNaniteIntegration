749bf072e50c49c1d8ca7d30f70291e5e5912f65
Ultime_Cookie777 <trove.jumpx@gmail.com>
2025-10-23 22:46:55 +0200

    testing bug fix

--- lua/Autorun/init.lua
+++ lua/Autorun/init.lua
@@ -9,14 +9,15 @@ NTNan.Path = table.pack(...)[1]
 Timer.Wait(function() if NTC ~= nil and NTC.RegisterExpansion ~= nil then NTC.RegisterExpansion(NTNan) end end,1)
 
 if (Game.IsMultiplayer and SERVER) or not Game.IsMultiplayer then
-    dofile(NTNan.Path.."/Lua/Scripts/afflictions.lua")
-    dofile(NTNan.Path.."/Lua/Scripts/helperfunctions.lua")
-    dofile(NTNan.Path.."/Lua/Scripts/humanupdate.lua")
+
+    Timer.Wait(function()
+        dofile(NTNan.Path.."/Lua/Scripts/afflictions.lua")
+        dofile(NTNan.Path.."/Lua/Scripts/helperfunctions.lua")
+        dofile(NTNan.Path.."/Lua/Scripts/humanupdate.lua")
+
+        NTC.AddHumanUpdateHook(NTNan.UpdateHuman)
+    end, 1)
+    
 else
     -- client only
 end
-
-
-
-
-NTC.AddHumanUpdateHook(NTNan.UpdateHuman)
