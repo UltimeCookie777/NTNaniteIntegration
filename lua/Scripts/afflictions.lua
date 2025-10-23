aa374704d46ca9461a0f8ee0f61199d79a42f6bd
Ultime_Cookie777 <trove.jumpx@gmail.com>
2025-10-23 14:12:39 +0200

    added isLocal variable

--- lua/Scripts/afflictions.lua
+++ lua/Scripts/afflictions.lua
@@ -7,7 +7,7 @@ NTNan.Afflictions.Reconstructor = "reconstructornanite"
 NTNan.Afflictions.DeepFix = "deepfixnanite"
 NTNan.Afflictions.BoneFix = "bonefixnanite"
 NTNan.Afflictions.OxyBlood = "oxybloodnanite"
-NTNan.Afflictions.Husk = "huskparalnanite"
+NTNan.Afflictions.Husk = "husknanite"
 NTNan.Afflictions.Neural = "neuralnanite"
 NTNan.Afflictions.AntiTox = "antitoxnanite"
 NTNan.Afflictions.VigorBuff = "vigorbuffnanite"
@@ -16,19 +16,19 @@ NTNan.Afflictions.VisionBuff = "visionbuffnanite"
 NTNan.Afflictions.LaniusXeno = "laniusxenonanite"
 
 NTNan.AfflictionsRes[NTNan.Afflictions.Reconstructor] = { name = NTNan.Afflictions.Reconstructor, levels = {
-    {{"burn", 0.3},{"acidburn",0.15},{"lacerations",0.3}},
-    {{"bleeding",0.5},{"bleedingnonstop",0.5},{"bitewounds",0.3}},
-    {{"gunshotwounds",0.2}}}
+    {{"burn", 0.3, true},{"acidburn",0.15, true},{"lacerations",0.3, true}},
+    {{"bleeding",0.5, true},{"bleedingnonstop",0.5, true},{"bitewounds",0.3, true}},
+    {{"gunshotwounds",0.2, true}}}
 }
 
 NTNan.AfflictionsRes[NTNan.Afflictions.BoneFix] = { name = NTNan.Afflictions.BoneFix, levels = {
-    {{"blunttrauma", 0.4},{"ra_fracture", 3}, {"la_fracture", 3}},
+    {{"blunttrauma", 0.4, true},{"ra_fracture", 10}, {"la_fracture", 3}},
     {{"rl_fracture", 3},{"ll_fracture", 3},{"t_fracture",3}},
-    {{"n_fracture", 3},{"h_fracture",3},{"t_paralysis",1},{"bonedama",0.5}}}
+    {{"n_fracture", 3},{"h_fracture",3},{"t_paralysis",1},{"bonedamage",0.5, true}}}
 }
 
 NTNan.AfflictionsRes[NTNan.Afflictions.DeepFix] = { name = NTNan.Afflictions.DeepFix, levels = {
-    {{"internalbleeding", 0.25},{"foreignbody", 0.2}},
+    {{"internalbleeding", 0.25},{"foreignbody", 0.2, true}},
     {{"organdamage", 0.1},{"liverdamage",0.05},{"heartdamage",0.05},{"lungdamage",0.05},{"kidneydamage",0.05},{"liverdamage",0.05}}}
 }
 
