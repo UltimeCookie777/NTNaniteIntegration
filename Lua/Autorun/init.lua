9445f98fc763d219aff53ea93247344e02289b72
ZeRat25 <zeratul@europe.com>
2025-10-26 14:07:57 +0100

    si j'ai encore forgor un truc je me tire une balle

--- Lua/Autorun/init.lua
+++ Lua/Autorun/init.lua
@@ -1,7 +1,7 @@
 
 NTNan = {} -- Neurotrauma Nanite Integration
 NTNan.Name="Nanite Integration"
-NTNan.Version = "1.1.1"
+NTNan.Version = "1.1.2"
 NTNan.VersionNum = 01100100
 NTNan.MinNTVersion = "A1.14.7"
 NTNan.MinNTVersionNum = 01140700
